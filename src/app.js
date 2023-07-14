e3494a11d8f554d9ee927ad0208166c6c5c9e28f
Tomasmusic <135050579+Tomasmusic@users.noreply.github.com>
2023-07-14 19:11:17 +0000

    random card 14 julio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//window.onload = function() {
+//write your code here
+//let pintas =[&#9824,&#9829,&#9827,&#9830; ]
+//let seleccion = document.querySelector("#pinta1")
+//pintas.innerHTML = pintas[0]
+//console.log("Hello Rigo from the console!");
+
+window.onload = () => {
+  //document.querySelector(".pinta").innerHTML = randomPinta();
+  document.querySelector(".pinta").innerHTML = randomPinta();
+  document.querySelector(".numero").innerHTML = randomNumero();
+};
+
+let randomNumero = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumeros];
+};
+
+let randomPinta = () => {
+  let pintas = ["&#9824", "&#9829", "&#9827", "&#9830"];
+  let indexpinta = Math.floor(Math.random() * pintas.length);
+  return pintas[indexpinta];
 };
